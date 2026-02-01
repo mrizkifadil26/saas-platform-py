059ed60b0491e1610c57e49a0d96c2a106d5007b
Muhammad Rizki Fadillah <mrizkifadil26@gmail.com>
2026-02-01 21:05:11 +0700

    feat(db): refactor user retrieval methods for improved readability

--- packages/db/src/db/repo/app_users/user_repo.py
+++ packages/db/src/db/repo/app_users/user_repo.py
@@ -12,21 +12,30 @@ class UserRepo:
 
     async def get_user_by_id(self, user_id: uuid.UUID) -> User | None:
         # query = "SELECT * FROM users WHERE id = :user_id"
-        query = select(User).where(User.id == user_id)
-        result = await self.db.execute(query, {"user_id": user_id})
+        stmt = (
+            select(User)
+            .where(User.id == user_id)
+        )
+        res = await self.db.execute(stmt)
 
-        # return result.fetchone()
-        return result.scalar_one_or_none()
+        return res.scalar_one_or_none()
 
     async def get_user_by_email(self, email: str) -> User | None:
         # query = "SELECT * FROM users WHERE email = :email"
-        query = select(User).where(User.email == email)
-        result = await self.db.execute(query, {"email": email})
+        stmt = (
+            select(User)
+            .where(User.email == email)
+        )
+        res = await self.db.execute(stmt)
 
-        # return result.fetchone()
-        return result.scalar_one_or_none()
+        return res.scalar_one_or_none()
 
-    async def create_user(self, email: str, full_name: str | None = None) -> User:
+    async def create_user(
+        self,
+        *,
+        email: str,
+        full_name: str | None = None,
+    ) -> User:
         u = User(email=email, full_name=full_name)
 
         self.db.add(u)
