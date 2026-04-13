20f0db81708fc81291a656b945a992a8537cfb9d
Muhammad Rizki Fadillah <mrizkifadil26@gmail.com>
2026-04-13 08:33:27 +0700

    feat(billing): update __all__ exports to include new service functions and reorganize entries

--- packages/billing/src/billing/__init__.py
+++ packages/billing/src/billing/__init__.py
@@ -1,20 +1,28 @@
-from .charge import charge_credits
+from .charge import ConsumeCreditsResult, charge_credits, consume_credits
 from .errors import BillingError, IdempotencyConflict, InsufficientCredits, UnknownPlan
+from .models import Wallet
 from .payg.plans import PaygPlan, get_payg_plan
+from .payg.service import grant_payg_credits
 from .subscription.plans import SubscriptionPlan, get_subscription_plan
+from .subscription.service import grant_subscription_credits
 from .types import Credits, PlanCode, RequestId
 
 __all__ = [
     "BillingError",
+    "ConsumeCreditsResult",
+    "Credits",
     "IdempotencyConflict",
     "InsufficientCredits",
-    "UnknownPlan",
-    "PaygPlan",
-    "get_payg_plan",
-    "SubscriptionPlan",
-    "get_subscription_plan",
-    "Credits",
     "PlanCode",
+    "PaygPlan",
     "RequestId",
+    "SubscriptionPlan",
+    "UnknownPlan",
+    "Wallet",
     "charge_credits",
+    "consume_credits",
+    "get_payg_plan",
+    "get_subscription_plan",
+    "grant_payg_credits",
+    "grant_subscription_credits",
 ]
