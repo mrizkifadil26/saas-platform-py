4a4d95aedc4c81765cf8a8275eb105ca152b32fe
Muhammad Rizki Fadillah <mrizkifadil26@gmail.com>
2026-05-12 18:19:47 +0700

    refactor: restructure application layer to ports-based architecture

---
+++ packages/iam/src/iam/shared/application/ports/uow.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from db.app_db import AppTransactionManager
+
+
+class UnitOfWork(AppTransactionManager):
+    pass
--- packages/iam/src/iam/shared/application/uow.py
+++
@@ -1,9 +0,0 @@
-from typing import Protocol
-
-
-class UnitOfWork(Protocol):
-    async def commit(self) -> None:
-        raise NotImplementedError
-
-    async def rollback(self) -> None:
-        raise NotImplementedError
