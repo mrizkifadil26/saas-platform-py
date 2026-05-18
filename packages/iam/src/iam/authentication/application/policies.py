fd91587adf41fd2fb794cb9cfbc62a0312fd6bfd
Muhammad Rizki Fadillah <mrizkifadil26@gmail.com>
2026-05-18 13:18:09 +0700

    refactor(authn): simplify authentication policy responsibilities

--- packages/iam/src/iam/authentication/application/policies.py
+++ packages/iam/src/iam/authentication/application/policies.py
@@ -1,34 +1,19 @@
-from datetime import datetime, timedelta
-
-from iam.authentication.domain import (
-    AuthenticationAttemptRepository,
-)
-from iam.identity.domain.value_objects import UserId
+from datetime import timedelta
 
 
-class LoginRateLimitPolicy:
-    MAX_ATTEMPTS = 5
+class AuthenticationPolicy:
+    MAX_FAILURES = 5
     TIME_WINDOW_MINUTES = 15
 
-    def __init__(
+    def ensure_not_locked(
         self,
-        repository: AuthenticationAttemptRepository,
-    ) -> None:
-        self._repository = repository
-
-    async def is_locked(
-        self,
-        user_id: UserId,
         *,
-        now: datetime,
-    ) -> bool:
-        since = now - timedelta(
-            minutes=self.TIME_WINDOW_MINUTES,
-        )
+        recent_failures: int,
+    ) -> None:
+        if recent_failures >= self.MAX_FAILURES:
+            # TODO: raise too many authentication attempts error
+            # raise TooManyAuthenticationAttemptsError()
+            raise
 
-        failures = await self._repository.count_failures_since(
-            user_id=user_id,
-            since=since,
-        )
-
-        return failures >= self.MAX_ATTEMPTS
+    def failure_window(self) -> timedelta:
+        return timedelta(minutes=self.TIME_WINDOW_MINUTES)
