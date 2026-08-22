671b29f2271d1b24d4eb3ab00e201d244d657e38
Muhammad Rizki Fadillah <mrizkifadil26@gmail.com>
2026-08-22 13:53:06 +0700

    remove legacy tests

--- packages/shared/db/tests/integration/test_app_db_base_metadata.py
+++
@@ -1,5 +0,0 @@
-from db.app_db.base import AppBase
-
-
-def test_app_base_has_metadata() -> None:
-    assert AppBase.metadata is not None
--- packages/shared/db/tests/integration/test_app_db_engine.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncEngine
-
-
-def test_create_app_engine_returns_async_engine(app_engine: AsyncEngine) -> None:
-    assert isinstance(app_engine, AsyncEngine)
