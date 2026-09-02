3ff4846c27eb0c7295bb37be6f4558b554355552
Muhammad Rizki Fadillah <mrizkifadil26@gmail.com>
2026-09-02 08:05:09 +0700

    refactor(identity): use dataclass instead plain init in use case

--- packages/iam/src/iam/identity/application/use_cases.py
+++ packages/iam/src/iam/identity/application/use_cases.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from iam.identity.domain import (
     EmailVerification,
     EmailVerificationRepository,
@@ -24,32 +26,25 @@ from .exceptions import (
     UserEmailAlreadyVerifiedError,
     UserNotFoundError,
 )
-from .interfaces import EmailVerificationTokenGenerator, EmailVerificationTokenHasher
+from .ports import EmailVerificationTokenGenerator, EmailVerificationTokenHasher
 
 
+@dataclass(slots=True)
 class RegisterUserUseCase:
-    def __init__(
-        self,
-        user_repository: UserRepository,
-        verification_repository: EmailVerificationRepository,
-        token_generator: EmailVerificationTokenGenerator,
-        token_hasher: EmailVerificationTokenHasher,
-        clock: Clock,
-    ):
-        self._user_repository = user_repository
-        self._verification_repository = verification_repository
-        self._token_generator = token_generator
-        self._token_hasher = token_hasher
-        self._clock = clock
+    user_repository: UserRepository
+    verification_repository: EmailVerificationRepository
+    token_generator: EmailVerificationTokenGenerator
+    token_hasher: EmailVerificationTokenHasher
+    clock: Clock
 
     async def execute(
         self,
         command: RegisterUserCommand,
     ) -> RegisterUserResult:
-        now = self._clock.now()
+        now = self.clock.now()
         email = Email(command.email)
 
-        existing_user = await self._user_repository.find_by_email(email)
+        existing_user = await self.user_repository.find_by_email(email)
         if existing_user is not None:
             raise UserAlreadyExistsError(email.value)
 
@@ -58,8 +53,8 @@ class RegisterUserUseCase:
             registered_at=now,
         )
 
-        raw_token = self._token_generator.generate()
-        token_hash = self._token_hasher.hash(raw_token)
+        raw_token = self.token_generator.generate()
+        token_hash = self.token_hasher.hash(raw_token)
 
         verification = EmailVerification.create(
             user_id=user.id,
@@ -68,13 +63,13 @@ class RegisterUserUseCase:
             ttl_minutes=15,
         )
 
-        await self._user_repository.save(user)
-        await self._verification_repository.save(verification)
+        await self.user_repository.save(user)
+        await self.verification_repository.save(verification)
 
         return RegisterUserResult(
             user=UserDTO(
-                id=user.id.unwrap(),
-                email=user.email.unwrap(),
+                id=user.id.value,
+                email=user.email.value,
                 is_verified=user.is_email_verified,
                 created_at=user.created_at,
             ),
@@ -83,46 +78,38 @@ class RegisterUserUseCase:
         )
 
 
+@dataclass(slots=True)
 class VerifyEmailUseCase:
-    def __init__(
-        self,
-        user_repository: UserRepository,
-        verification_repository: EmailVerificationRepository,
-        token_hasher: EmailVerificationTokenHasher,
-        clock: Clock,
-    ):
-        self._user_repository = user_repository
-        self._verification_repository = verification_repository
-        self._token_hasher = token_hasher
-        self._clock = clock
+    user_repository: UserRepository
+    verification_repository: EmailVerificationRepository
+    token_hasher: EmailVerificationTokenHasher
+    clock: Clock
 
     async def execute(
         self,
         command: VerifyEmailCommand,
     ) -> EmailVerificationResult:
-        now = self._clock.now()
+        now = self.clock.now()
 
         raw_token = EmailVerificationToken(command.token)
-        token_hash = self._token_hasher.hash(raw_token)
+        token_hash = self.token_hasher.hash(raw_token)
 
-        verification = await self._verification_repository.find_by_token_hash(
-            token_hash
-        )
+        verification = await self.verification_repository.find_by_token_hash(token_hash)
         if verification is None:
             raise InvalidEmailVerificationTokenError()
 
         if verification.is_expired(now):
             raise EmailVerificationExpiredError()
 
-        user = await self._user_repository.find_by_id(verification.user_id)
+        user = await self.user_repository.find_by_id(verification.user_id)
         if user is None:
             raise UserNotFoundError()
 
         user.mark_email_as_verified(verified_at=now)
         verification.mark_verified(verified_at=now)
 
-        await self._user_repository.save(user)
-        await self._verification_repository.save(verification)
+        await self.user_repository.save(user)
+        await self.verification_repository.save(verification)
         # TODO: uow commit here
 
         return EmailVerificationResult(
@@ -137,36 +124,30 @@ class VerifyEmailUseCase:
 
 
 class ResendEmailVerificationUseCase:
-    def __init__(
-        self,
-        user_repository: UserRepository,
-        verification_repository: EmailVerificationRepository,
-        token_generator: EmailVerificationTokenGenerator,
-        token_hasher: EmailVerificationTokenHasher,
-        clock: Clock,
-    ):
-        self._user_repository = user_repository
-        self._verification_repository = verification_repository
-        self._token_generator = token_generator
-        self._token_hasher = token_hasher
-        self._clock = clock
+    user_repository: UserRepository
+    verification_repository: EmailVerificationRepository
+
+    token_generator: EmailVerificationTokenGenerator
+    token_hasher: EmailVerificationTokenHasher
+
+    clock: Clock
 
     async def execute(
         self,
         command: ResendEmailVerificationCommand,
     ) -> None:
-        now = self._clock.now()
+        now = self.clock.now()
         user_id = UserId(command.user_id)
 
-        user = await self._user_repository.find_by_id(user_id)
+        user = await self.user_repository.find_by_id(user_id)
         if user is None:
             raise UserNotFoundError()
 
         if user.is_email_verified:
             raise UserEmailAlreadyVerifiedError()
 
-        raw_token = self._token_generator.generate()
-        token_hash = self._token_hasher.hash(raw_token)
+        raw_token = self.token_generator.generate()
+        token_hash = self.token_hasher.hash(raw_token)
 
         verification = EmailVerification.create(
             user_id=user.id,
@@ -174,7 +155,7 @@ class ResendEmailVerificationUseCase:
             created_at=now,
         )
 
-        await self._verification_repository.save(verification)
+        await self.verification_repository.save(verification)
 
 
 class DeactivateUserUseCase:
